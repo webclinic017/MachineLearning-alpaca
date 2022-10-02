052e2304f8ee81b9c240b69961e7e9073b26f2aa
EliTheKnight <e.sodickson@gmail.com>
2022-10-02 10:53:49 -0400

    Added rsme and mape functions

--- DatasetAnalysis.py
+++ DatasetAnalysis.py
@@ -31,16 +31,42 @@ def read_data():
     return pandas.read_csv('data.csv')
 
 
-# get closing price for each day
-data = read_data()[['close']]
-# reverses pandas data
-data = data.iloc[::-1]
+def extract_seqX_outcomeY(data, N, offset):
 
-#sets up test sets
-test_ratio = 0.2
-train_ratio = 1 - test_ratio
-train_size = int(train_ratio * len(data))
-test_size = int(test_ratio * len(data))
-train = data[:train_size]
-test = data[train_size:]
+    X, y = [], []
 
+    for i in range(offset, len(data)):
+        X.append(data[i-N:i])
+        y.append(data[i])
+
+    return  np.array(X), np.array(y)
+
+
+def calculate_rmse(y_true, y_pred):
+
+    rmse = np.sqrt(np.mean((y_true - y_pred) ** 2))
+
+    return rmse
+
+
+def calculate_mape(y_true, y_pred):
+
+    y_true, y_pred = np.array(y_true), np.array(y_pred)
+    mape = np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+
+    return mape
+
+
+if __name__ == '__main__':
+    # get closing price for each day
+    data = read_data()[['close']]
+    # reverses pandas data
+    data = data.iloc[::-1]
+
+    # sets up test sets
+    test_ratio = 0.2
+    train_ratio = 1 - test_ratio
+    train_size = int(train_ratio * len(data))
+    test_size = int(test_ratio * len(data))
+    train = data[:train_size]
+    test = data[train_size:]
