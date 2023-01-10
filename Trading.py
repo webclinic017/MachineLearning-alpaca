c40042b24601dc7e60d9d861a8470dcff5e0353f
EliTheKnight <e.sodickson@gmail.com>
2023-01-10 09:23:17 -0500

    added function descriptions

--- Trading.py
+++ Trading.py
@@ -5,6 +5,49 @@ import pyotp
 from dotenv import load_dotenv
 
 
+def get_my_stocks():
+    """
+    gets dict of all owned stocks
+    :return: dict of all owned stocks
+    """
+    return r.build_holdings()
+
+
+def get_stocks_current_price(stocks: Union[str, list]):
+    """
+    gets most recent price of stock(s)
+    :param stocks: stock symbol or list of stock symbols
+    :return: current/latest price of stock(s)
+    """
+    return r.stocks.get_latest_price(stocks)
+
+
+def buy_stock(ticker: str, price: float):
+    """
+    Buys stock by price
+    :param ticker: str, stock symbol to buy
+    :param price: float, amount in $ to buy
+    :return: dict of order id and information
+    """
+    response = r.order_buy_fractional_by_price(symbol=ticker, amountInDollars=price)
+    return response
+
+
+def sell_stock(ticker: str, price: float = None):
+    """
+    Sells stock by price
+    :param ticker: str, stock symbol to sell
+    :param price: float, amount in $ to sell, if None than it sells all owned stock
+    :return: dict of order id and information
+    """
+    if price is None:
+        total = get_my_stocks()[ticker]['price']
+    else:
+        total = price
+    response = r.order_sell_fractional_by_price(ticker, total)
+    return response
+
+
 class Trader:
 
     def __init__(self):
@@ -17,40 +60,5 @@ class Trader:
         # print("Current OTP:", totp)
         # login
         self.login = r.login(username, password, mfa_code=totp)
-
-    def get_my_stocks(self):
-        """
-        :return: dict of all owned stocks
-        """
-        return r.build_holdings()
-
-    def get_stocks_current_price(self, stocks: Union[str, list]):
-        """
-        :param stocks: stock symbol or list of stock symbols
-        :return: current/latest price of stock(s)
-        """
-        return r.stocks.get_latest_price(stocks)
-
-    def buy_stock(self, ticker: str, price: float):
-        """
-        :param ticker: str, stock symbol to buy
-        :param price: float, amount in $ to buy
-        :return: dict of order id and information
-        """
-        response = r.order_buy_fractional_by_price(symbol=ticker, amountInDollars=price)
-        return response
-
-    def sell_stock(self, ticker: str, price: float = None):
-        """
-        :param ticker: str, stock symbol to sell
-        :param price: float, amount in $ to sell, if None than it sells all owned stock
-        :return: dict of order id and information
-        """
-        if price is None:
-            total = self.get_my_stocks()[ticker]['price']
-        else:
-            total = price
-        response = r.order_sell_fractional_by_price(ticker, total)
-        return response
 
 
