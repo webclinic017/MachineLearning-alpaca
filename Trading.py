191ab8ccae9ce29845b45a9623b6725a1b15e427
Eli Sodickson <e.sodickson@gmail.com>
2023-02-15 09:59:15 -0500

    fixed GFD error

--- Trading.py
+++ Trading.py
@@ -41,7 +41,7 @@ def buy_stock(ticker: str, price: float):
         response = r.order_buy_fractional_by_price(symbol=ticker, amountInDollars=price)
     if response is not None and 'id' not in response:    # try again if didn't work
         time.sleep(5)
-        response = r.order_buy_fractional_by_price(symbol=ticker, amountInDollars=price')
+        response = r.order_buy_fractional_by_price(symbol=ticker, amountInDollars=price)
 
     return response
 
